1d7b7886175a3349595c9828f12e354435025131
Leeyanhawrt <leeyan1108@gmail.com>
2023-09-23 14:57:23 -0400

    Add getStaticPaths function

--- pages/[pid].js
+++ pages/[pid].js
@@ -30,7 +30,17 @@ export async function getStaticProps(context) {
       loadedProduct: product
     }
   }
+}
 
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { pid: 'p1' } },
+      { params: { pid: 'p2' } },
+      { params: { pid: 'p3' } },
+    ],
+    fallback: false
+  };
 }
 
 export default ProductDetailPage;
