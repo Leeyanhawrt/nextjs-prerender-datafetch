f7baa390443616a09e17bc3c716fb1ca9955e359
Leeyanhawrt <leeyan1108@gmail.com>
2023-09-24 14:00:08 -0400

    Learn about getserversideprops

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -7,6 +7,8 @@ function UserProfilePage({ username }) {
 export default UserProfilePage;
 
 export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
   return {
     props: {
       username: 'Max'
